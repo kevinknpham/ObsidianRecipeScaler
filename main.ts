7cfa3235e2740cfeb8a30003128cf1df095a1e7a
Kevin Pham <kevinknpham@gmail.com>
2023-06-26 00:41:57 -0700

    first commit

--- main.ts
+++ main.ts
@@ -1,72 +1,40 @@
-import { App, Editor, MarkdownView, Modal, Notice, Plugin, PluginSettingTab, Setting } from 'obsidian';
+import { App, Editor, MarkdownRenderChild, MarkdownView, Modal, Notice, Plugin, PluginSettingTab, Setting } from 'obsidian';
 
-// Remember to rename these classes and interfaces!
+const SCALE_FACTOR_PATTERN = /^(x|\/)\d+$/;
+const SCALABLE_QUANTITY_PATTERN = /^((\d+((\.\d+)|( \d+\/\d+))?)|(\d+\/\d+))( [a-zA-Z ]+)?$/;
 
-interface MyPluginSettings {
-	mySetting: string;
+interface RecipeScalerSettings {
+	scaleFactor: string;
 }
 
-const DEFAULT_SETTINGS: MyPluginSettings = {
-	mySetting: 'default'
+const DEFAULT_SETTINGS: RecipeScalerSettings = {
+	scaleFactor: 'x1'
 }
 
-export default class MyPlugin extends Plugin {
-	settings: MyPluginSettings;
+export default class RecipeScaler extends Plugin {
+	settings: RecipeScalerSettings;
 
 	async onload() {
 		await this.loadSettings();
 
-		// This creates an icon in the left ribbon.
-		const ribbonIconEl = this.addRibbonIcon('dice', 'Sample Plugin', (evt: MouseEvent) => {
-			// Called when the user clicks the icon.
-			new Notice('This is a notice!');
-		});
-		// Perform additional things with the ribbon
-		ribbonIconEl.addClass('my-plugin-ribbon-class');
+		const scaleFactor = this.getScaleFactorFromString(this.settings.scaleFactor);
 
-		// This adds a status bar item to the bottom of the app. Does not work on mobile apps.
-		const statusBarItemEl = this.addStatusBarItem();
-		statusBarItemEl.setText('Status Bar Text');
+		this.registerMarkdownPostProcessor((element, context) => {
+			const codeblocks = element.querySelectorAll("code");
 
-		// This adds a simple command that can be triggered anywhere
-		this.addCommand({
-			id: 'open-sample-modal-simple',
-			name: 'Open sample modal (simple)',
-			callback: () => {
-				new SampleModal(this.app).open();
-			}
-		});
-		// This adds an editor command that can perform some operation on the current editor instance
-		this.addCommand({
-			id: 'sample-editor-command',
-			name: 'Sample editor command',
-			editorCallback: (editor: Editor, view: MarkdownView) => {
-				console.log(editor.getSelection());
-				editor.replaceSelection('Sample Editor Command');
-			}
-		});
-		// This adds a complex command that can check whether the current state of the app allows execution of the command
-		this.addCommand({
-			id: 'open-sample-modal-complex',
-			name: 'Open sample modal (complex)',
-			checkCallback: (checking: boolean) => {
-				// Conditions to check
-				const markdownView = this.app.workspace.getActiveViewOfType(MarkdownView);
-				if (markdownView) {
-					// If checking is true, we're simply "checking" if the command can be run.
-					// If checking is false, then we want to actually perform the operation.
-					if (!checking) {
-						new SampleModal(this.app).open();
-					}
+      for (let index = 0; index < codeblocks.length; index++) {
+        const codeblock = codeblocks.item(index);
+        const text = codeblock.innerText.trim();
 
-					// This command will only show up in Command Palette when the check function returns true
-					return true;
-				}
-			}
-		});
+        if (SCALABLE_QUANTITY_PATTERN.test(text)) {
+					context.addChild(new ScaledQuantity(codeblock, text, scaleFactor))
+        }
+      }
+
+    });
 
 		// This adds a settings tab so the user can configure various aspects of the plugin
-		this.addSettingTab(new SampleSettingTab(this.app, this));
+		this.addSettingTab(new ScalingSettingsTab(this.app, this));
 
 		// If the plugin hooks up any global DOM events (on parts of the app that doesn't belong to this plugin)
 		// Using this function will automatically remove the event listener when this plugin is disabled.
@@ -81,6 +49,14 @@ export default class MyPlugin extends Plugin {
 	onunload() {
 
 	}
+	
+	getScaleFactorFromString(rawScaleFactor : string) : number {
+		if (rawScaleFactor[0] === 'x') {
+			return +rawScaleFactor.substring(1);
+		} else {
+			return 1 / (+rawScaleFactor.substring(1));
+		}
+	}
 
 	async loadSettings() {
 		this.settings = Object.assign({}, DEFAULT_SETTINGS, await this.loadData());
@@ -91,26 +67,10 @@ export default class MyPlugin extends Plugin {
 	}
 }
 
-class SampleModal extends Modal {
-	constructor(app: App) {
-		super(app);
-	}
+class ScalingSettingsTab extends PluginSettingTab {
+	plugin: RecipeScaler;
 
-	onOpen() {
-		const {contentEl} = this;
-		contentEl.setText('Woah!');
-	}
-
-	onClose() {
-		const {contentEl} = this;
-		contentEl.empty();
-	}
-}
-
-class SampleSettingTab extends PluginSettingTab {
-	plugin: MyPlugin;
-
-	constructor(app: App, plugin: MyPlugin) {
+	constructor(app: App, plugin: RecipeScaler) {
 		super(app, plugin);
 		this.plugin = plugin;
 	}
@@ -120,18 +80,222 @@ class SampleSettingTab extends PluginSettingTab {
 
 		containerEl.empty();
 
-		containerEl.createEl('h2', {text: 'Settings for my awesome plugin.'});
+		containerEl.createEl('h2', {text: 'Settings for Recipe Scaler.'});
 
 		new Setting(containerEl)
-			.setName('Setting #1')
-			.setDesc('It\'s a secret')
-			.addText(text => text
-				.setPlaceholder('Enter your secret')
-				.setValue(this.plugin.settings.mySetting)
-				.onChange(async (value) => {
-					console.log('Secret: ' + value);
-					this.plugin.settings.mySetting = value;
-					await this.plugin.saveSettings();
-				}));
+      .setName("Scaling factor")
+      .setDesc("Factor by which to scale recipes.  Should be x or / followed by an integer.")
+      .addText((text) =>
+        text
+          .setPlaceholder("x1")
+          .setValue(this.plugin.settings.scaleFactor)
+          .onChange(async (value) => {
+						if (SCALE_FACTOR_PATTERN.test(value)) {
+							this.plugin.settings.scaleFactor = value;
+							await this.plugin.saveSettings();
+						}
+          })
+      );
+
+	}
+}
+
+interface UnitConversion {
+	from : string,
+	to : string,
+	multiplier : number 
+}
+
+class ScaledQuantity extends MarkdownRenderChild {
+	static VOLUME_UNITS_IN_TSP : Record<string, number> = {
+		tsp: 1,
+		tbsp: 3,
+		cup: 3 * 16,
+		gallon: 3 * 16 * 16
+	};
+
+	static MASS_UNITS_IN_GRAMS : Record<string, number> = {
+		g: 1,
+		kg: 1000
+	}
+
+	static ALT_NAMES : Record<string, string> = {
+		teaspoon: 'tsp',
+		teaspoons: 'tsp',
+		tablespoon: 'tbsp',
+		tablespoons: 'tbsp',
+		cups: 'cup',
+		gallons: 'gallon',
+		gram: 'g',
+		grams: 'g',
+		kilogram: 'kg',
+		kilograms: 'kg'
+	}
+
+	static PLURAL_FORMS : Record<string, string> = {
+		cup: 'cups',
+		gallon: 'gallons'
+	}
+
+	text: string;
+	scaleFactor: number;
+
+	constructor(containerEl: HTMLElement, text: string, scaleFactor: number) {
+		super(containerEl);
+
+		this.text = text;
+		this.scaleFactor = scaleFactor;
+	}
+
+	onload(): void {
+		let newTextEl;
+		if (/^\d+(\.\d+)?( [a-zA-Z ]+)?$/g.test(this.text)) {
+			newTextEl = this.handleDecimal();
+		} else {
+			newTextEl = this.handleMixedNumber();
+		}
+		this.containerEl.replaceWith(newTextEl);
+	}
+
+	handleDecimal() : HTMLElement {
+		const spaceIndex = this.text.indexOf(' ');
+		const amount = spaceIndex == -1 ? +this.text : +this.text.substring(0, spaceIndex);
+		const unit = spaceIndex == -1 ? '' : this.text.substring(spaceIndex + 1);
+
+		const scaledAmount = amount * this.scaleFactor;
+		const unitAliased = ScaledQuantity.ALT_NAMES[unit] ?? unit;
+
+		let finalUnit = unit;
+		let finalAmount = scaledAmount;
+		const unitConversion = this.getDesiredUnit(scaledAmount, unitAliased) ?? unit;
+		if (unitConversion.to !== unitAliased) {
+			finalUnit = unitConversion.to;
+			finalAmount *= unitConversion.multiplier;
+		}
+
+		const finalText = this.roundToTwo(finalAmount) + (finalUnit.length == 0 ? '' : ` ${finalUnit}`);
+
+		return this.containerEl.createSpan({text : finalText});
+	}
+	
+	handleMixedNumber() : HTMLElement {
+		let amountWhole = 0;
+		let fractionString = '';
+		let unit = '';
+		if (/^\d+ \d+\/\d+?( [a-zA-Z ]+)?$/.test(this.text)) {
+			const firstSpaceIndex = this.text.indexOf(' ');
+			const secondSpaceIndex = this.text.indexOf(' ', firstSpaceIndex + 1);
+
+			amountWhole = +this.text.substring(0, firstSpaceIndex);
+			fractionString = secondSpaceIndex == -1 ? this.text.substring(firstSpaceIndex + 1) : this.text.substring(firstSpaceIndex + 1, secondSpaceIndex);
+			unit = secondSpaceIndex == -1 ? '' : this.text.substring(secondSpaceIndex + 1);
+		} else {
+			const spaceIndex = this.text.indexOf(' ');
+			fractionString = spaceIndex == -1 ? this.text : this.text.substring(0, spaceIndex);
+			unit = spaceIndex == -1 ? '' : this.text.substring(spaceIndex + 1);
+		}
+
+		console.table({amountWhole, fractionString, unit});
+
+		const [numerator, denominator] : number[] = fractionString.split('/').map(el => +el);
+
+		console.table({numerator, denominator});
+
+		const currTotalFractionNumerator : number = amountWhole * denominator + numerator;
+		const scaledNumerator = currTotalFractionNumerator * this.scaleFactor;
+
+		console.table({currTotalFractionNumerator, scaledNumerator});
+
+		const unitAliased = ScaledQuantity.ALT_NAMES[unit] ?? unit;
+
+		let finalUnit = unit;
+		let convertedNumerator = scaledNumerator;
+		const unitConversion = this.getDesiredUnit(scaledNumerator / denominator, unitAliased) ?? unit;
+		if (unitConversion.to !== unitAliased) {
+			finalUnit = unitConversion.to;
+			convertedNumerator *= unitConversion.multiplier;
+		}
+
+		const [finalNumerator, finalDenominator] : number[] = this.reduceFraction(convertedNumerator, denominator);
+
+		console.table({
+			finalDenominator,
+			finalNumerator
+		})
+
+		let finalAmountText;
+		if (finalDenominator === 1) {
+			finalAmountText = finalNumerator;
+		} else {
+			finalAmountText = `${Math.floor(finalNumerator / finalDenominator)} ${finalNumerator % finalDenominator}/${finalDenominator}`;
+		}
+
+		const finalText = finalAmountText + (finalUnit.length == 0 ? '' : ` ${finalUnit}`);
+
+		return this.containerEl.createSpan({text : finalText});
+	}
+
+	reduceFraction(origNumerator : number, origDenominator : number) : number[] {
+		const getGCD = function(n : number, d : number) : number {
+			let numerator = (n<d)?n:d;
+			let denominator = (n<d)?d:n;        
+			let remainder = numerator;
+			let lastRemainder = numerator;
+
+			while (true){
+				console.table({n, d})
+					lastRemainder = remainder;
+					remainder = denominator % numerator;
+					if (remainder === 0){
+							break;
+					}
+					denominator = numerator;
+					numerator = remainder;
+			}
+			if(lastRemainder){
+					return lastRemainder;
+			}
+			return 0;
+		};
+
+		const gcd = getGCD(origNumerator, origDenominator);
+
+		return [origNumerator / gcd, origDenominator / gcd];
+	}
+
+	roundToTwo(num : number) : number {
+    return +(Math.round(+(num + "e+2"))  + "e-2");
+	}
+
+	getDesiredUnit(amount : number, currentUnit : string) : UnitConversion {
+		if (ScaledQuantity.VOLUME_UNITS_IN_TSP[currentUnit]) {
+			const amountInTsp = ScaledQuantity.VOLUME_UNITS_IN_TSP[currentUnit] * amount;
+
+			const entries = Object.entries(ScaledQuantity.VOLUME_UNITS_IN_TSP)
+					.sort((a,b) => b[1] - a[1]);
+			for (let i = 0; i < entries.length; i++) {
+				const [unit, tspInUnit] = entries[i];
+				if (amountInTsp / tspInUnit >= 1) {
+					return {from: currentUnit, to: unit, multiplier: ScaledQuantity.VOLUME_UNITS_IN_TSP[currentUnit] / tspInUnit};
+				}
+			}
+			return {from: currentUnit, to: 'tsp', multiplier: ScaledQuantity.VOLUME_UNITS_IN_TSP[currentUnit]};
+		}
+
+		if (ScaledQuantity.MASS_UNITS_IN_GRAMS[currentUnit]) {
+			const amountInGrams = ScaledQuantity.MASS_UNITS_IN_GRAMS[currentUnit] * amount;
+
+			const entries = Object.entries(ScaledQuantity.MASS_UNITS_IN_GRAMS)
+					.sort((a,b) => b[1] - a[1]);
+			for (let i = 0; i < entries.length; i++) {
+				const [unit, gramsInUnit] = entries[i];
+				if (amountInGrams / gramsInUnit > 1) {
+					return {from: currentUnit, to: unit, multiplier: ScaledQuantity.MASS_UNITS_IN_GRAMS[currentUnit] / gramsInUnit};
+				}
+			}
+			return {from: currentUnit, to: 'g', multiplier: ScaledQuantity.MASS_UNITS_IN_GRAMS[currentUnit]};
+		}
+
+		return {from: currentUnit, to: currentUnit, multiplier: 1};
 	}
 }
